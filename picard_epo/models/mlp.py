82128981e8935c103229edeae94487808b3e57cf
Andrew Zheng <azheng92@gmail.com>
2025-09-29 10:00:51 -0700

    BatchMLP

--- picard_epo/models/mlp.py
+++ picard_epo/models/mlp.py
@@ -1,18 +1,30 @@
 import flax.linen as nn
 
 
-class Linear(nn.Module):
+class BatchMLP(nn.Module):
     """
-    Linear model for predicting c from x.
-    Accepts inputs B x n_items x dim
+    Batch version of MLP for predicting c from x.
+    Accepts inputs B x (n_items x dim)
     and outputs B x n_items
     """
 
+    hidden_sizes: list
     dim: int
+    output_size: int = 1
+    squeeze: bool = True
 
     @nn.compact
     def __call__(self, x):
-        x = nn.Dense(1)(x.reshape(x.shape[0], -1, self.dim)).squeeze()
+        # x is of shape (B, n_items * dim)
+        batch_size = x.shape[0]
+        x = x.reshape((batch_size, -1, self.dim))  
+        for size in self.hidden_sizes:
+            x = nn.Dense(size)(x)
+            x = nn.relu(x)
+        x = nn.Dense(self.output_size)(x)  # Output layer predicts c values
+        x = x.reshape(batch_size, -1, self.output_size)  # Reshape back to (B, n_items, output_size)
+        if self.squeeze and self.output_size == 1:
+            x = x.squeeze(-1)
         return x
 
 
